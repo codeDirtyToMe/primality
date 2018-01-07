a761384528a941be44b78f05c29c37c8c4979981
codeDirtyToMe <32625084+codeDirtyToMe@users.noreply.github.com>
2018-01-07 02:30:49 -0500

    Update primeDetermination.py

--- primeDetermination.py
+++ primeDetermination.py
@@ -12,28 +12,44 @@
 #    own list for output at the end of the program.
 #   -Added a bunch of comments so that I don't forget what the hell I've been doing.
 
+#January 6-7, 2018
+#   -Started getting the multi-threading ready.
+#   -Timed the execution w/o threading for numbers 1-100,000. It took 3.66715407371521 seconds.
+#   -Hopefully, after implementing at least two threads I'll see a shorter time.
+#   -Ability to write primes to a file for comparison was added.
+#   -After outputting primes to file, I ran "$diff primeNumbers.txt confirmedPrimes.txt -wq" and
+#       confirmed that they are in fact equivalent.
+
 #To do:
-#       1: Compare the first 100k prime numbers to a list of confirmed prime numbers in order to
-#          check the validity of the program.
-#       2: Add multi-threading for crunching really large numbers.
-#           -Will I have to have threads leap frog each other? This should be fun.
+#       1: (DONE) Compare the first 100k prime numbers a list of confirmed prime numbers in order to
+#          check the validity of the program. $>diff file1 file2
+#       2: (STARTED) Add multi-threading for crunching really large numbers.
+#          Will I have to have threads leap frog each other? This should be fun.
 #       3: Possibly implement this as a function into my polymorphic cipher that I haven't
 #          worked on in a couple months.
 
-import math
+import math, time, os, threading
 
-num = 1 #Start at this number. Change if desired.
+num = 2       #Start at this number. Change if desired.
+endNum = 100000  #End at this number. Change if desired.
 nonPrimeNumbers = []
 primeNumbers = []
 
-for s in range(10000) : #How many numbers to check the primality of.
+#evenThread = threading.Thread(target = , args = [])
+#evenThread.start()
+#oddThread = threading.Thread(target = , args = [])
+#oddThread.start()
+
+#Let's time this as a single thread.
+startTime = time.time() #Start time recorded.
+for s in range(endNum) : #How many numbers to check the primality of.
     # Set the ceiling of the numbers to check based on the floor of the sqrt of the num value.
     ceiling = math.floor(math.sqrt(num))
     while ceiling >= 1 :
         remainder = num % ceiling #Check for remainder.
         if remainder == 0 : #No remainder.
             if ceiling == 1 : #Is divisible by 1.
-                #num is prime because it wouldn't get to a ceiling value of 1 unless it wasn't divisible by any thing larger.
+                #num is prime because it wouldn't get to a ceiling of 1 unless it wasn't divisible by anything larger.
                 primeNumbers.append(str(num))
                 ceiling = 0 #Set ceiling to a value that will break the while loop.
             else :
@@ -46,7 +62,38 @@ for s in range(10000) : #How many numbers to check the primality of.
     s += 1 #Increase the for loop counter.
     num += 1 #Test the next number in the range.
 
-#Print out the results.
-print("Not Prime: " + ",".join(nonPrimeNumbers))
-print("Prime: " + ",".join(primeNumbers))
+stopTime = time.time() #Stop time recorded
+totalTime = stopTime - startTime
+
+print("Execution took: " + str(totalTime) + str(" seconds."))
+print("Formatting found primes for file comparision...")
+#Format results to match the list  of confirmed prime numbers. I suppose I could do this easier in the loop above.
+#However, I suspect that would hinder the performance; not that I'm all that worried about that until I start
+#the multi-threading. Whatever, we'll make it a learning experience. That and I actually enjoy doing this stuff.
+#10 across. \t between each. last value ends with \n.
+
+#Open the file to output the data to, or create the file if it doesn't already exist.
+if os.path.exists("primeNumbers.txt"):
+    primeFile = open("/home/malmach/PycharmProjects/primeNumbers/primeNumbers.txt", "w")
+else :
+    primeFile = open("primeNumbers.txt", "w")
+
+#Set up some counters.
+a = 0
+b = 0
+#Start adding tabs and newline feeds.
+while a <= math.ceil(endNum / 10) : #Essentially, how many lines are there?
+    if len(primeNumbers[b:]) >= 10 : #If there are currently more than ten left.
+        primeFile.write("\t".join(primeNumbers[b:b + 10]) + "\n")
+    elif (len(primeNumbers[b:]) < 10) and (len(primeNumbers[b:]) > 0) : #If there are < 10 && > 0 values left.
+        primeFile.write("\t".join(primeNumbers[b:]) + "\n")
+    else :
+        break #No values left, break the loop.
+
+    b += 10 #Shift 10 indices to the right.
+    a += 1  #Next line.
+
+primeFile.close()
+print("Now, if one were to \"$diff confirmedPrimes.txt primeNumbers.txt -wq\", one should see no return. This means\ "
+      "that there is a reasonable chance that this program is working as intended.")
 exit(0)
